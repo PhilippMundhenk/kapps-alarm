d0723eed4a21f79c58678a09d5437da5d9ef7ccd
Philipp Mundhenk <philipp@mundhenk.org>
2020-11-26 21:54:26 +0100

    empty app created

--- alarm/alarm.py
+++ alarm/alarm.py
@@ -1,11 +1,25 @@
+import os
+
+appFolder = None
+
+
 def register():
+    global appFolder
     print("register Alarm")
+    path = os.path.dirname(os.path.realpath(__file__))
+    appFolder = path.split("kapps")[1]
     return {"icon": "res/icon.png", "name": "Alarm"}
 
 
 def handle(path):
     if "/res" in path:
+        # app resource requested
         return {"code": 200, "content": getRes(path.replace("/", "", 1))}
+    elif path.split(appFolder)[1] == "":
+        # app is started
+        return {"code": 200, "content": "<html><h1>Alarm App</h1></html>"}
+    else:
+        return {"code": 404, "content": "<html><h1>Not Found</h1></html>"}
 
 
 def getRes(path):
