1e67c25fd6db809307fd5e23e338fb00414dacd7
Philipp Mundhenk <philipp@mundhenk.org>
2020-11-26 21:52:39 +0100

    initial commit

--- alarm/alarm.py
+++ alarm/alarm.py
@@ -1,2 +1,13 @@
 def register():
     print("register Alarm")
+    return {"icon": "res/icon.png", "name": "Alarm"}
+
+
+def handle(path):
+    if "/res" in path:
+        return {"code": 200, "content": getRes(path.replace("/", "", 1))}
+
+
+def getRes(path):
+    with open(path, 'r') as file:
+        return file.read()
