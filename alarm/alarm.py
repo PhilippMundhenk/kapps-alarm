8cc91cfe2eb4de3af8cc35be15db1a8bf224f9b2
Philipp Mundhenk <philipp@mundhenk.org>
2020-11-27 11:49:27 +0100

    updated

--- alarm/alarm.py
+++ alarm/alarm.py
@@ -1,27 +1,39 @@
-import os
+class Kapp():
+    icon = "res/icon.png"
+    name = "Alarm"
 
-appFolder = None
+    def __init__(self, appID, appPath, ctx):
+        self.appID = appID
+        self.appPath = appPath
+        self.ctx = ctx
+
+    def getAppPythonPath(self):
+        return self.appPath
+
+    def getAppFSPath(self):
+        return self.appPath.replace(".", "/") + "/"
+
+    def getAppURL(self):
+        return "/apps/" + str(self.appID)
+
+    def urlToAppPath(self, url):
+        return url.replace(self.getAppURL(), self.getAppFSPath())
+
+    def getRes(self, path):
+        with open(path, 'r') as file:
+            return file.read()
+
+    def handleGet(self, path):
+        if "/res" in path:
+            # app resource requested
+            return {"code": 200, "content": self.getRes(self.urlToAppPath(path))}
+        elif path.split(appFolder)[1] == "":
+            # app is started
+            return {"code": 200, "content": "<html><h1>Alarm App</h1></html>"}
+        else:
+            return {"code": 404, "content": "<html><h1>Not Found</h1></html>"}
 
 
-def register():
-    global appFolder
+def register(appID, appPath, ctx):
     print("register Alarm")
-    path = os.path.dirname(os.path.realpath(__file__))
-    appFolder = path.split("kapps")[1]
-    return {"icon": "res/icon.png", "name": "Alarm"}
-
-
-def handle(path):
-    if "/res" in path:
-        # app resource requested
-        return {"code": 200, "content": getRes(path.replace("/", "", 1))}
-    elif path.split(appFolder)[1] == "":
-        # app is started
-        return {"code": 200, "content": "<html><h1>Alarm App</h1></html>"}
-    else:
-        return {"code": 404, "content": "<html><h1>Not Found</h1></html>"}
-
-
-def getRes(path):
-    with open(path, 'r') as file:
-        return file.read()
+    return Kapp(appID, appPath, ctx)
