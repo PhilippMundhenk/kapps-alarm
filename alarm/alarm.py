9c7c704923e59f24a86f4b464d8537b44c2735b1
Philipp Mundhenk <philipp@mundhenk.org>
2020-12-01 11:20:49 +0100

    Refactored Kapp class

--- alarm/alarm.py
+++ alarm/alarm.py
@@ -1,27 +1,9 @@
-class Kapp():
+from core import kapp
+
+
+class Alarm(kapp.Kapp):
     icon = "res/icon.png"
     name = "Alarm"
-
-    def __init__(self, appID, appPath, ctx):
-        self.appID = appID
-        self.appPath = appPath
-        self.ctx = ctx
-
-    def getAppPythonPath(self):
-        return self.appPath
-
-    def getAppFSPath(self):
-        return self.appPath.replace(".", "/") + "/"
-
-    def getAppURL(self):
-        return "/apps/" + str(self.appID)
-
-    def urlToAppPath(self, url):
-        return url.replace(self.getAppURL(), self.getAppFSPath())
-
-    def getRes(self, path):
-        with open(path, 'r') as file:
-            return file.read()
 
     def handleGet(self, path):
         if "/res" in path:
@@ -35,5 +17,5 @@ class Kapp():
 
 
 def register(appID, appPath, ctx):
-    print("register " + Kapp.name)
-    return Kapp(appID, appPath, ctx)
+    print("register " + Alarm.name)
+    return Alarm(appID, appPath, ctx)
