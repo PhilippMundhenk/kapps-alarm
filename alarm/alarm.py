74ea6b5c3c918d1d90d0d7c134a822a938559365
Philipp Mundhenk <philipp@mundhenk.org>
2020-12-04 12:26:41 +0100

    implemented new APIs

--- alarm/alarm.py
+++ alarm/alarm.py
@@ -1,12 +1,13 @@
 from core.kapp import Kapp
+from core.httpResponse import HTTPResponse
 
 
 class Alarm(Kapp):
     name = "Alarm"
 
-    def iconCallback(self):
-        return {"code": 200, "content": self.getRes("icon.png")}
-        
+    def iconCallback(self, kcommand):
+        return HTTPResponse(content=self.getRes("icon.png"))
+
 
 def register(appID, appPath, ctx):
     print("register " + Alarm.name)
